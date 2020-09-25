b39fd2296f0d6992744b6d63316f4b65b2ec7cc1
Mert <mertdemirok80@gmail.com>
2020-09-25 18:41:28 +0200

    add permission and notification

--- utils/helpers.js
+++ utils/helpers.js
@@ -1,3 +1,48 @@
+import { AsyncStorage } from "react-native";
+import * as Notifications from "expo-notifications";
+import * as Permissions from "expo-permissions";
+
+const NOTIFICATION_KEY = "FlashCardApp:notifications";
+
+export function clearLocalNotification() {
+  return AsyncStorage.removeItem(NOTIFICATION_KEY).then(
+    Notifications.cancelAllScheduledNotificationsAsync
+  );
+}
+
+export function setLocalNotification() {
+  AsyncStorage.getItem(NOTIFICATION_KEY)
+    .then(JSON.parse)
+    .then((data) => {
+      if (data === null) {
+        Permissions.askAsync(Permissions.NOTIFICATIONS).then(
+          async ({ status }) => {
+            if (status === "granted") {
+              Notifications.cancelAllScheduledNotificationsAsync();
+
+              let tomorrow = new Date();
+              tomorrow.setDate(tomorrow.getDate() + 1);
+              tomorrow.setHours(20);
+              tomorrow.setMinutes(0);
+
+              Notifications.scheduleNotificationAsync({
+                content: {
+                  title: "Flashcard Daily Reminder!",
+                  body: "👋 You didn't take any quiz today",
+                  data: { data: "goes here" },
+                },
+                trigger: { seconds: (tomorrow.getTime() - Date.now()) / 1000 },
+              });
+
+              AsyncStorage.setItem(NOTIFICATION_KEY, JSON.stringify(true));
+            } else {
+            }
+          }
+        );
+      }
+    });
+}
+
 export const result = (score) => {
   if (score < 50) {
     return "Poor ;(";
